b865e330597ada687f82e40e9d313d4cc443381b
unknown <edebaks@lsbu.ac.uk>
2023-05-27 13:12:52 +0100

    First commit

---
+++ application/views/users/templates/footer.php
@@ -0,0 +1,16 @@
+
+  <footer class="main-footer">
+    <div class="pull-right hidden-xs">
+      <b>FINAL</b> YEAR
+    </div>
+    <strong>&copy; 2022 <?php echo date('Y'); ?> |</strong> Samet Edebak - London South Bank University
+  </footer>
+
+  <!-- Add the sidebar's background. This div must be placed
+       immediately after the control sidebar -->
+  <div class="control-sidebar-bg"></div>
+</div>
+<!-- ./wrapper -->
+
+</body>
+</html>
